033a398424f7d5f5683c25286f8f0d8702b38b94
Benjamin Murden <ben.murden@gmail.com>
2016-09-21 17:24:53 +0900

    Add credits generation

--- getall.py
+++ getall.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 import urllib2
 import json
 import os
@@ -14,6 +15,7 @@ with open('prefectures.json', 'w') as out:
     out.close()
 
 data = json.loads(content)
+credits_content = '# Photo Credits\n'
 
 def make_sure_path_exists(path):
     try:
@@ -24,11 +26,19 @@ def make_sure_path_exists(path):
 
 for prefecture in data:
     make_sure_path_exists(prefecture['name_en'])
+    credits_content += '## {name_en} ({name_jp})\n'.format(name_en=prefecture['name_en'], name_jp=prefecture['name'])
 
     for image in prefecture['images']:
-        response = urllib2.urlopen('{prot}://{domain}{path}'.format(prot=protocol, domain=domain, path=image['path']))
+        image_url = '{prot}://{domain}{path}'.format(prot=protocol, domain=domain, path=image['path'])
+        response = urllib2.urlopen(image_url)
         filename = image['path'].split('/')[-1].strip()
+        credits_content += '### [{name_en} ({name_jp})]({link})\n'.format(name_en=image['name_en'], name_jp=image['name'], link=image_url)
+        credits_content += 'Photo credit: {} ({})\n'.format(image['photographer_name_en'], image['photographer_name'])
 
         with open('{dir}/{file}'.format(dir=prefecture['name_en'], file=filename), 'wb') as out:
             out.write(response.read())
             out.close()
+
+with open('CREDITS.md', 'w') as out:
+    out.write(credits_content.encode('utf8'))
+    out.close()
